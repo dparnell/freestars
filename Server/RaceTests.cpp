2bfead52fe1ab812796091965775635cbf1e3072
Daniel Parnell <me@danielparnell.com>
2022-06-05 11:45:50 +1000

    Added another test - also fails :(

--- Server/RaceTests.cpp
+++ Server/RaceTests.cpp
@@ -16,23 +16,41 @@ class DummyMessageSink : public FreeStars::MessageSink {
 	}
 };
 
-void testJoatAdvantagePoints()
-{
-	ALEPH_TEST_BEGIN("test advantage points calculation for JOAT");
+auto fs = cmrc::test_data::get_filesystem();
 
-	auto fs = cmrc::test_data::get_filesystem();
+DummyMessageSink messageSink;
+FreeStars::Game game;
 
-	DummyMessageSink messageSink;
-
+void setup() {
 	TiXmlDocument rulesDoc;
 	auto rules_xml = fs.open("test_data/MyModRules.xml");
 	rulesDoc.Parse((const char*)rules_xml.cbegin());
 	auto def = rulesDoc.FirstChild("RulesDefinition");
 
-	FreeStars::Game game;
 	FreeStars::Rules::LoadRules(def->FirstChild("Rules"), "test_data", "", 1.0, messageSink);
 	FreeStars::RacialTrait::LoadRacialTraits(&game, def->FirstChild("RacialTraits"));
 	game.LoadComponents(def->FirstChild("Components"));
+}
+
+void testImmuneJoatAdvantagePoints()
+{
+	ALEPH_TEST_BEGIN("test advantage points calculation for JOAT");
+
+	TiXmlDocument raceDoc;
+	auto race_xml = fs.open("test_data/immune-joat.r0");
+	raceDoc.Parse((const char*)race_xml.cbegin());
+	FreeStars::Race r;
+	r.ParseNode(raceDoc.FirstChildElement("RaceDefinition"), false, messageSink);
+
+	long points = r.GetAdvantagePoints(messageSink);
+	ALEPH_ASSERT_EQUAL(points, -3900); // value as calculated by Stars! 2.70
+
+	ALEPH_TEST_END();
+}
+
+void testJoatAdvantagePoints()
+{
+	ALEPH_TEST_BEGIN("test advantage points calculation for JOAT");
 
 	TiXmlDocument raceDoc;
 	auto race_xml = fs.open("test_data/default.r0");
@@ -41,12 +59,15 @@ void testJoatAdvantagePoints()
 	r.ParseNode(raceDoc.FirstChildElement("RaceDefinition"), false, messageSink);
 
 	long points = r.GetAdvantagePoints(messageSink);
-	ALEPH_ASSERT_EQUAL(points, 25);
+	ALEPH_ASSERT_EQUAL(points, 25); // value as calculated by Stars! 2.70
 
 	ALEPH_TEST_END();
 }
 
 int main(int, char**)
 {
+	setup();
+
+	testImmuneJoatAdvantagePoints();
 	testJoatAdvantagePoints();
 }
